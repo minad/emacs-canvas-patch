f3de7a8621ff9a9d28573947ea969e37aa152e00
Daniel Mendler <mail@daniel-mendler.de>
2026-02-15 04:46:25 +0100

    Update ob-canvas

--- ob-canvas.el
+++ ob-canvas.el
@@ -5,17 +5,18 @@
 (require 'ob)
 
 (defvar ob-canvas-count 0)
+(defvar ob-canvas-start (float-time))
 (defvar ob-canvas-handle '(image :type canvas :canvas-width 300 :canvas-height 300 :canvas-id ob-canvas))
 (defvar ob-canvas-timer (run-with-timer
-                         nil (/ 1 100.0)
+                         nil (/ 1 60.0)
                          (lambda ()
                            (when (fboundp 'ob-canvas-render)
-                             (ob-canvas-render ob-canvas-handle)))))
+                             (ob-canvas-render ob-canvas-handle (- (float-time) ob-canvas-start))))))
 
 (defun org-babel-execute:canvas (body params)
   (font-lock-add-keywords nil `((": {CANVAS}" 0 '(face nil display ,ob-canvas-handle))) t)
   (let ((file (format "/tmp/ob-canvas%s.so" ob-canvas-count)))
-    (shell-command (format "gcc -DEXP=%s -O2 -I%ssrc ob-canvas.c -o %s -fPIC -shared"
+    (shell-command (format "gcc -DEXP=%s -O3 -I%ssrc ob-canvas.c -o %s -fPIC -shared"
                            (shell-quote-argument (string-clean-whitespace body))
                            (shell-quote-argument source-directory)
                            (shell-quote-argument file)))
