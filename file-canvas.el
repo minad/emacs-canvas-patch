3c3c0fa36283536d081dd1c5d61a2aab247df7c8
Daniel Mendler <mail@daniel-mendler.de>
2026-07-07 20:01:14 +0200

    Fix file-canvas.el test

--- file-canvas.el
+++ file-canvas.el
@@ -1,6 +1,6 @@
 ;;; Read a file containing ARGB32 binary pixel data and create a canvas from it -*- lexical-binding: t -*-
 
-(setq test-canvas (create-image "file-canvas.data"
+(setq test-canvas (create-image (expand-file-name "file-canvas.data")
                                 'canvas nil
                                 :data-width 200
                                 :data-height 50))
