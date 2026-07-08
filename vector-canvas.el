05c27a0741d048f6be6c8bc74b9e9b7d3b7d61f6
Daniel Mendler <mail@daniel-mendler.de>
2026-07-08 14:13:55 +0200

    test two canvas objects

--- vector-canvas.el
+++ vector-canvas.el
@@ -1,13 +1,22 @@
 ;;; Create a canvas from a vector, and refresh it from Lisp -*- lexical-binding: t -*-
 
-(setq test-canvas `(image :type canvas
-                          :data-width 200
-                          :data-height 100
-                          :data ,(make-vector (* 200 100) #xFFFF0000)))
-(insert (propertize "#" 'display test-canvas))
+(setq test-canvas1 `(image :type canvas
+                           :data-width 200
+                           :data-height 100
+                           :data ,(make-vector (* 200 100) #xFFFF0000)))
+
+(setq test-canvas2 `(image :type canvas
+                           :data-width 200
+                           :data-height 100
+                           :data ,(make-vector (* 200 100) #xFF0000FF)))
+
+(insert (propertize "#" 'display test-canvas1))
+(insert (propertize "#" 'display test-canvas2))
 
 (run-at-time 1.0 nil (lambda ()
                        (dotimes (i 100)
-                         (aset (plist-get (cdr test-canvas) :data) (+ i (* 200 i)) #xFFFFFFFF)
-                         (canvas-refresh test-canvas t)
+                         (aset (plist-get (cdr test-canvas1) :data) (+ i (* 200 i)) #xFFFFFFFF)
+                         (aset (plist-get (cdr test-canvas2) :data) (+ i (* 200 i)) #xFFFFFFFF)
+                         (canvas-refresh test-canvas1 t)
+                         (canvas-refresh test-canvas2 t)
                          (redisplay))))
