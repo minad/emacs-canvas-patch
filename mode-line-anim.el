cc42ec77a7683dc631cc949965e8438423ab34ad
Daniel Mendler <mail@daniel-mendler.de>
2026-08-09 10:35:33 +0200

    Improve mode line anim, no force-mode-line-update, separate function

--- mode-line-anim.el
+++ mode-line-anim.el
@@ -9,39 +9,41 @@
           :data ,anim-vec
           :ascent center))
 (defvar mode-line-anim-timer nil)
-
-(setq-default mode-line-format
-              (cons '(:eval (propertize "#" 'display mode-line-anim-canvas)) mode-line-format))
+(defvar mode-line-anim-tt 0.0)
 
 (defun mode-line-anim-start ()
   (interactive)
   (when (timerp mode-line-anim-timer) (cancel-timer mode-line-anim-timer))
-  (let ((tt 0.0))
-    (setq mode-line-anim-timer
-      (run-with-timer
-       0 0.07
-       (lambda ()
-         (dotimes (y 16)
-           (dotimes (x 16)
-             (let* ((v (/ (+ (sin (+ (/ x 2.5) tt))
-                              (sin (+ (/ y 2.5) (* tt 1.3)))
-                              (sin (/ (+ (sqrt (+ (* x x) (* y y))) (* tt 10)) 2.5)))
-                          3.0))
-                    (h (mod (/ (+ v 1) 2.0) 1.0))
-                    (i (floor (* h 6))) (f (- (* h 6) i)))
-               (aset anim-vec (+ (* y 16) x)
-                     (pcase i
-                       (0 (logior #xFF000000 #xFF0000 (ash (round (* f 255)) 8)))
-                       (1 (logior #xFF000000 (ash (round (* (- 1 f) 255)) 16) #x00FF00))
-                       (2 (logior #xFF000000 #x00FF00 (round (* f 255))))
-                       (3 (logior #xFF000000 (ash (round (* (- 1 f) 255)) 8) #x0000FF))
-                       (4 (logior #xFF000000 (ash (round (* f 255)) 16) #x0000FF))
-                       (_ (logior #xFF000000 #xFF0000 (round (* (- 1 f) 255)))))))))
-         (canvas-refresh mode-line-anim-canvas t)
-         (setq tt (+ tt 0.15)))))))
+  (setq mode-line-anim-timer
+        (run-with-timer 0 0.07 #'mode-line-anim-update)))
+
+(defun mode-line-anim-update ()
+  (dotimes (y 16)
+    (dotimes (x 16)
+      (let* ((tt mode-line-anim-tt)
+             (v (/ (+ (sin (+ (/ x 2.5) tt))
+                      (sin (+ (/ y 2.5) (* tt 1.3)))
+                      (sin (/ (+ (sqrt (+ (* x x) (* y y))) (* tt 10)) 2.5)))
+                   3.0))
+             (h (mod (/ (+ v 1) 2.0) 1.0))
+             (i (floor (* h 6))) (f (- (* h 6) i)))
+        (aset anim-vec (+ (* y 16) x)
+              (pcase i
+                (0 (logior #xFF000000 #xFF0000 (ash (round (* f 255)) 8)))
+                (1 (logior #xFF000000 (ash (round (* (- 1 f) 255)) 16) #x00FF00))
+                (2 (logior #xFF000000 #x00FF00 (round (* f 255))))
+                (3 (logior #xFF000000 (ash (round (* (- 1 f) 255)) 8) #x0000FF))
+                (4 (logior #xFF000000 (ash (round (* f 255)) 16) #x0000FF))
+                (_ (logior #xFF000000 #xFF0000 (round (* (- 1 f) 255)))))))))
+  (canvas-refresh mode-line-anim-canvas t)
+  (setq mode-line-anim-tt (+ mode-line-anim-tt 0.15)))
 
 (defun mode-line-anim-stop ()
   (interactive)
   (when (timerp mode-line-anim-timer)
     (cancel-timer mode-line-anim-timer)
     (setq mode-line-anim-timer nil)))
+
+(add-to-list 'mode-line-format
+             '(:eval (propertize "#" 'display mode-line-anim-canvas)))
+(mode-line-anim-start)
